7ee31c62adc3e87f6e0fb08d03d8b4ac31e3aede
VietLH <vietlh@sgss-MacBook-Pro-2.local>
2020-08-02 23:34:56 +0700

    update asset files

--- HieuLuat/GeneralSettings.swift
+++ HieuLuat/GeneralSettings.swift
@@ -43,7 +43,7 @@ class GeneralSettings {
     private static var adsOptout = false //true means the user will not see Ads
     
     //these settings must be updated before making build
-    private static var requiredDBVersion = 9
+    private static var requiredDBVersion = 10
     private static var developementMode = false
     private static var defaultActiveQC41Id = 7 //this would be used for the search of querying the lastest road sign
     private static var defaultActiveNDXPId = 6 //this would be used for the search of querying the latest NDXP
